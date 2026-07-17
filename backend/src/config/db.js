7030637a9690a173188fa1b8c6ab262b5c64dfe0
vinayak-kumar-12 <vinayakkumar709@gmail.com>
2026-07-17 12:48:03 +0530

    Add DATABASE_URL and SSL support to PostgreSQL pool config in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,8 +7,11 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 
 pool.connect(async (err, client, release) => {
   if (err) {
